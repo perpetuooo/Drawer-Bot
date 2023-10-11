87c067665818b21cb2c3e9a6c4fae68a873e7fd1
Pedro <pedrohenriqueperpetuo@gmail.com>
2023-10-10 21:12:16 -0300

    Update settings.py

--- AutoDrawer/settings.py
+++ AutoDrawer/settings.py
@@ -3,11 +3,11 @@ import datetime
 
 #configuration of global variables.
 class Config():
-    date = datetime.datetime.now().strftime("%d-%m-%y %H.%M.%S")
-    imgs_path = Path.home() / "Pictures" / "src"
+    img_date = datetime.datetime.now().strftime("%d-%m-%y %H.%M.%S")
+    imgs_path = Path.home() / "Pictures" / "AutoDrawer"
 
     file_path = None
-    canvas_up = None
-    canvas_down = None
+    canvasY = None
+    canvasX = None
 
 stg = Config()
