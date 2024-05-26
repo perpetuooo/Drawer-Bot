35fc1eca0f6507cd7ef8ea1c10e6fa848f85aa0c
Pedro <pedrohenriqueperpetuo@gmail.com>
2024-05-25 22:03:57 -0300

    better logic

--- AutoDrawer/browser.py
+++ AutoDrawer/browser.py
@@ -1,120 +1,118 @@
-from selenium import webdriver
-from settings import stg
 from PIL import Image
-import io
+from pathlib import Path
+from datetime import datetime
+from selenium import webdriver
+
+import requests
+import psutil
+import random
 import re
 import os
-import time
-import random
-import psutil
-import requests
-
-
-#detecting the users browser and adjusting its settings
-def detect_browser():
-    for process in psutil.process_iter(attrs=['pid', 'name']):
-        try:
-            if "chrome" in process.info['name'].lower():
-                options = webdriver.ChromeOptions()
-                options.add_argument("headless")
-                options.add_argument("--log-level=3")
-                driver = webdriver.Chrome(options=options)
-                break
-            elif "firefox" in process.info['name'].lower():
-                options = webdriver.FirefoxOptions()
-                options.add_argument('-headless')
-                options.add_argument("--log-level=error")
-                driver = webdriver.Firefox(options=options)
-                break
-            elif "msedge" in process.info['name'].lower():
-                options = webdriver.EdgeOptions()
-                options.use_chromium = True
-                options.add_argument("headless")
-                options.add_argument("--log-level=3")
-                driver = webdriver.Edge(options=options)
-                break
-            elif "safari" in process.info['name'].lower():
-                options = webdriver.SafariOptions()
-                options.add_argument("headless")
-                options.add_argument("--log-level=3")
-                driver = webdriver.Safari(options=options)
-                break
-        
-        except Exception as e:
-            break
-
-    return driver
-
-
-#chooses a random image and tries to download it.
-def img_download(img, file_name): 
-    try:
-        if not os.path.exists(stg.imgs_path):
-            os.makedirs(stg.imgs_path)
-
-        img_content = requests.get(img).content
-        img_file = io.BytesIO(img_content)
-        stg.file_path = stg.imgs_path / file_name
-        pil = Image.open(img_file)
-
-        if pil.mode == 'P':
-            pil = pil.convert('RGB')
-
-        with open(stg.file_path, 'wb') as f:
-            pil.save(f, 'JPEG', quality=95)
-
-    #retrying the download function in case of errors.
-    except Exception as e:
-        max_retries = 5
-        retry_count = 0
-
-        while retry_count <= max_retries:
-            try:
-                img_download(img, file_name)
-                break
-
-            except Exception as e:
-                retry_count += 1
-
-        else:
-            return("( ! ) Something went wrong, try again.")
+import io
 
 
 #image scrapping through Google Images.
-def img_search(keyword):
-    try:
-        stg.file_path = None
-        driver = detect_browser()
-        driver.get(f'https:/www.google.com/search?tbm=isch&q= {keyword} clipart')
-        time.sleep(0.5)
+def img_search(keyword, path):
 
-        img_urls = []
-        img_name = f"{keyword} {stg.img_date}"
-        page_source = driver.page_source
+    #detecting the users browser and adjusting its settings
+    def detect_browser():
+        for process in psutil.process_iter(['name']):
+            try:
+                match process.info['name'].lower():
+                    case "chrome.exe":
+                        options = webdriver.ChromeOptions()
+                        #options.add_argument("headless")
+                        options.add_argument("--log-level=3")
+                        return webdriver.Chrome(options=options)
 
-        #finding all URLs that are images and putting them in the list.
-        regex = r'"(https://[^"]+\.(jpg|jpeg))",(\d+),(\d+)'
-        matches = re.findall(regex, page_source)
+                    case "firefox.exe":
+                        options = webdriver.FirefoxOptions()
+                        #options.add_argument('-headless')
+                        options.add_argument("--log-level=error")
+                        return webdriver.Firefox(options=options)
 
-        for url in matches:
-            url = url[0]
-            img_urls.append(url)
+                    case "msedge.exe":
+                        options = webdriver.FirefoxOptions()
+                        #options.add_argument('-headless')
+                        options.add_argument("--log-level=error")
+                        return webdriver.Firefox(options=options)
+
+                    case "safari.exe":
+                        options = webdriver.SafariOptions()
+                        #options.add_argument("headless")
+                        options.add_argument("--log-level=3")
+                        return webdriver.Safari(options=options)
+                    
+                    case _:
+                        pass
+            
+            except Exception as e:
+                print(f"error: {str(e)}")
+
+
+    #downloading the image.
+    def downloader(img, file_path):
+            try:
+                print("downloader")
+                if not os.path.exists(path):
+                    os.makedirs(path)
+
+                img_content = requests.get(img).content
+                img_file = io.BytesIO(img_content)
+                img_done = Image.open(img_file).convert('RGB')
+                img_done.save(file_path, "JPEG", quality=80)
+                print("DONE!")
+
+            #retrying the download function in case of errors.
+            except Exception as e:
+                print(f"error: {str(e)}")
+
+                max_retries = 5
+                retry_count = 0
+
+                """while retry_count <= max_retries:
+                    try:
+                        downloader(img, file_name)
+                        break
+
+                    except Exception:
+                        retry_count += 1
+
+                else:
+                    print("something went wrong...")"""
+
+
+    # last_keyword = None
+    driver = detect_browser()
+    if not driver:
+        print("driver not compatible...")
+        return
+
+    driver.get(f'https:/www.google.com/search?tbm=isch&q={keyword} clipart')
+
+    results = []
+    source = driver.page_source
+    regex = r'"(https://[^"]+\.(jpg|jpeg|png))",(\d+),(\d+)'
+
+    matches = re.findall(regex, source)
+
+    for url in matches:
+        results.append(url[0])
+
+    if results:
+        print("results")
+        chosen_img = random.choice(results)
+        date = datetime.now().strftime("%d-%m-%y %H.%M.%S")
+        img_name = f"{keyword} {date}.jpg"
+        file_path = os.path.join(path, img_name)
         
-        if img_urls:
-            rnd_img = random.choice(img_urls) 
-            img_download(rnd_img, f"{img_name}.jpg")
-
-        else:
-            return ("( ! ) No images found, try again\n")
+        downloader(chosen_img, file_path)
     
-    except Exception as e:
-        return (f"( - ) ERROR: {str(e)}")
+    else:
+        print("no imgs found...")
 
-    finally:
-        driver.quit()
-        return (f"{len(img_urls)} images were found!\nChoosen image --> {rnd_img}")
 
 
 
 if __name__ == '__main__':
-    pass
+    img_search("gecko", Path.home() / "Pictures" / "AutoDrawer")
