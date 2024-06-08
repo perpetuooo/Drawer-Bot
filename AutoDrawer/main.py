0c7079b82322d4df70f5dadbefcecec4edd6d789
Pedro <pedrohenriqueperpetuo@gmail.com>
2024-06-07 22:06:59 -0300

    Update main.py

--- AutoDrawer/main.py
+++ AutoDrawer/main.py
@@ -6,25 +6,41 @@ from resources import utils
 import keyboard
 import argparse
 import cmd2
+import sys
 import os
 
 class CLI(cmd2.Cmd):
     prompt = 'AD >> '
     intro = "Simple CLI"
 
-    #def do_help(self, line):
-
     
-    def precmd(self, line):
-        return line
+    def __init__(self):
+        super().__init__()
 
-    
-    def do_start(self, line):
+        self.last_keyword = None
+        self.add_settable(cmd2.Settable('last_keyword', str, 'last keyword', self))
+
+
+    def do_search(self, line):
+        '''given a keyword, search and donwload an equivalent image'''
         print(f"searching for {line} images...")
         img_search(line, CLI.default_path)
 
 
+    def do_load(self, line):
+        ''''''
+        print(self.last_keyword)
+        self.last_keyword = "whale"
+        print(self.last_keyword)
+
+
+    def do_start(self, line):
+        ''''''
+        pass
+
+
     def do_clear(self, line):
+        '''clear'''
         _ = call('clear' if os.name == 'posix' else 'cls')
 
 
@@ -36,4 +52,5 @@ class CLI(cmd2.Cmd):
 
 
 if __name__ == '__main__':
-    CLI().cmdloop()
+    c = CLI()
+    sys.exit(c.cmdloop())
